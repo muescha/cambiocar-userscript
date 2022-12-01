9c01df6d5b815e497d044f17d8678dc0ba716337
Muescha <184316+muescha@users.noreply.github.com>
2022-12-01 04:00:47 +0100

    new line

--- cambiocar-userscipt.user.js
+++ cambiocar-userscipt.user.js
@@ -109,6 +109,7 @@ function displayInline(cell, url) {
         function (currentValue, currentIndex, listObj) {
             var rideCell = currentValue.querySelectorAll("td")[1]
             var rideInfo = rideCell.textContent.split("\n").map((s) => s.trim().replace(/\t/g, '').replace(" -", ''));
+            
             var carClass = rideInfo[indexCar].match(regCarClass)[1]
             var start = rideInfo[indexStart].split(" ")
             var end = rideInfo[indexEnd].split(" ")
